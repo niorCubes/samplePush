3abb48c84e9727d5e6c6485b6f47da81192af6fd
nior cubes <larry.mabuti.jr@gmail.com>
2022-03-07 17:55:13 +0800

    v2

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 <body>
     <center>
         <h1>This is a sample scrip!!</h1>
+        <h1>This is a sample scrip!!</h1>
         <form action="includes/script.php" method="post" enctype="multipart/form-data">
             <input type="text" name="input1" id="input1" placeholder="Location" required>
             <input type="file" name="image" id="image">
